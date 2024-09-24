0bccdad87432ad8e8bf23b9bb40e65650add86fa
Francis Lagadia <francis.lagadia@emburse.com>
2024-09-24 10:21:58 -0700

    Add disabled file

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  { "rebelot/heirline.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+}
