e5283f130a8f814663b1a892b6a5af408fbaa5be
Francis Lagadia <francis.lagadia@emburse.com>
2024-09-24 10:23:56 -0700

    Add python and copilot support

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,5 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,5 +8,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   -- import/override with your plugins folder
 }
