be4fc0813ddc60d3d544b8cc3aacf49f88b1713c
Francis Lagadia <francis.lagadia@emburse.com>
2024-11-19 21:17:26 -0800

    read from pyproject.toml instead of specifying linter settings

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -19,12 +19,12 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
-      null_ls.builtins.formatting.isort.with {
-        extra_args = { "--profile", "black", "-l", "120" },
-      },
-      null_ls.builtins.formatting.black.with {
-        extra_args = { "-S", "-l", "120" },
-      },
+      -- null_ls.builtins.formatting.isort.with {
+      --   extra_args = { "--profile", "black", "-l", "120" },
+      -- },
+      -- null_ls.builtins.formatting.black.with {
+      --   extra_args = { "-S", "-l", "120" },
+      -- },
     })
   end,
 }
